0e3dc11f2d0039917c04e278ac23e7c8c069273b
Dmitry Bozhok <dmbozhok@gmail.com>
2020-07-25 09:06:41 +0700

    Add bootstrap-vue

--- gitpod-example/nuxt.config.js
+++ gitpod-example/nuxt.config.js
@@ -14,6 +14,10 @@ module.exports = {
     ]
   },
   /*
+  ** Bootstrap-vue section
+  */
+  modules: ['bootstrap-vue/nuxt'],
+  /*
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
